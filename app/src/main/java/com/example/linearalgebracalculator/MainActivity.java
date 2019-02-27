140773add0a254270628bdd0da81a45094851707
N0XIRE <connorwilson989@gmail.com>
2019-02-27 16:54:50 -0600

    Added matrix data entry and help text

--- app/src/main/java/com/example/linearalgebracalculator/MainActivity.java
+++ app/src/main/java/com/example/linearalgebracalculator/MainActivity.java
@@ -1,57 +1,25 @@
 package com.example.linearalgebracalculator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.Button;
 
-import java.util.ArrayList;
-import java.util.List;
-
-class MainActivity extends Activity implements OnItemSelectedListener{
+public class MainActivity extends Activity{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Spinner element
-        Spinner spinner = findViewById(R.id.calculateSelect);
-
-        // Spinner click listener
-        spinner.setOnItemSelectedListener(this);
-
-        // Spinner Drop down elements
-        List<String> categories = new ArrayList<>();
-        categories.add("Calculation 1");
-        categories.add("Calculation 2");
-        categories.add("Calculation 3");
-        categories.add("Calculation 4");
-        categories.add("Calculation 5");
-        categories.add("Calculation 6");
-
-        // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
-
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // attaching data adapter to spinner
-        spinner.setAdapter(dataAdapter);
     }
+    public void sendMessage(View view) {
+        Button btn = (Button)findViewById(R.id.toCalculate);
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        String item = parent.getItemAtPosition(position).toString();
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, calculate_Screen.class));
+            }
+        });
     }
 }
